dd38b17af0b9abeb14bb4450dc80b57e4b1b5e25
Andrea-LaVentura75 <laventuraandrea40@gmail.com>
2025-01-13 16:11:32 +0100

    first commit

---
+++ src/main/java/it/epicode/progetto/settimanale/prenotazioneViaggi/auth/AuthResponse.java
@@ -0,0 +1,11 @@
+package it.epicode.progetto.settimanale.prenotazioneViaggi.auth;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class AuthResponse {
+    private String token;
+
+}
---
+++ src/main/java/it/epicode/progetto/settimanale/prenotazioneViaggi/auth/Role.java
@@ -0,0 +1,6 @@
+package it.epicode.progetto.settimanale.prenotazioneViaggi.auth;
+
+public enum Role {
+    ROLE_USER,
+    ROLE_ADMIN
+}
